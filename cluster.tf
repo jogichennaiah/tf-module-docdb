b9d22adc21942e2ebf2d0d1f4e6fe8da14946b53
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-14 18:19:46 +0530

    Auto push

--- cluster.tf
+++ cluster.tf
@@ -9,6 +9,14 @@ resource "aws_docdb_cluster" "docdb" {
 
 }
 
+# Creates DOCDB Instances and adds to the cluster
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "roboshop-${var.ENV}-docdb"
+  cluster_identifier = aws_docdb_cluster.docdb.id
+  instance_class     = "db.t3.medium"
+}
+
 
 # Creates DocDB Subnet Group
 resource "aws_docdb_subnet_group" "docdb" {
