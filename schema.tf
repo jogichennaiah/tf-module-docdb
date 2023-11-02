d2008ccbc690a316a84aacc60079cbffedd569bb
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-02 22:42:58 +0530

    Auto push

--- schema.tf
+++ schema.tf
@@ -1,13 +1,17 @@
+# Injecting the schema
 resource "null_resource" "schema" {
-    depends_on = [aws_db_instance.mysql]
- provisioner "local-exec" {
-    command = <<EOF
-       cd /tmp
-       curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-       unzip mysql.zip
-       cd mysql-main
-       mysql -h ${aws_db_instance.mysql.address} -uadmin1 -proboshop1 <shipping.sql
-    EOF
-    
- }
- }
+
+  depends_on = [aws_docdb_cluster.docdb , aws_docdb_cluster_instance.cluster_instances]  
+  provisioner "local-exec" {
+         command = <<EOF
+            cd /tmp 
+            wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+            curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+            unzip -o /tmp/mongodb.zip 
+            cd mongodb-main
+            ls -ltr
+            mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile /tmp/global-bundle.pem --username admin1 --password roboshop1  < catalogue.js
+            mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile /tmp/global-bundle.pem --username admin1 --password roboshop1  < users.js
+        EOF 
+    }
+}
