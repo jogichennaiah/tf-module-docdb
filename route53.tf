058150bf157b35b3cde2734d7d36068d2b284732
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-05 18:52:49 +0530

    Auto push

--- route53.tf
+++ route53.tf
@@ -1,7 +1,8 @@
 resource  "aws_route53_record" "www" {
-    zone_id = aws_route53_zone.primary.zone_id
-    name    = "www.example.com"
-    type    = "A"
-    ttl     = 300
-    records  = [aws_eip.lb.public_ip] 
+    zone_id = HOSTEDZONE ID IS NEEDED
+    name    = componentName.privateDomain.com
+    type    = "CNAME"
+    ttl     = 10
+    records  = [] 
 }
+
