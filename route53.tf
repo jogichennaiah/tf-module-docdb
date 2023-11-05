fd9234ee6c58996b5d4ac35041c48591551e13a6
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-05 19:22:12 +0530

    Auto push

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource  "aws_route53_record" "record" {
     zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS 
-    name    = "mongodb.$(var.ENA).$(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_NAME)"
+    name    = "mongodb-$(var.ENA).$(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_NAME)"
     type    = "CNAME"
     ttl     = 10
     records  = [aws_docdb_cluster.docdb.endpoint] 
