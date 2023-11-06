5dd68aaac0fdaf78e7bae71c252290168f90be28
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-06 23:01:07 +0530

    Auto push

--- data.tf
+++ data.tf
@@ -8,3 +8,18 @@ data "terraform_remote_state" "vpc" {
         region  = "us-east-1" 
     }
 }
+
+# fetches the information of the secret
+data "aws_secretsmanager_secret" "secrets" {
+    name        = "robot/secrets"
+
+}
+
+#Fetches the secrets version from the above server
+data "aws_secretsmanger_secret_version" "secret_version" {
+    secret_id  = data.aws_secretsmanager_secret.example.id
+}
+
+output "example" {
+    value      = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
+}
