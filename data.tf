85b667cac5dfa8aa973c6d13f54e89d1e85e2063
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-06 23:57:22 +0530

    Auto push

--- data.tf
+++ data.tf
@@ -11,13 +11,12 @@ data "terraform_remote_state" "vpc" {
 
 # fetches the information of the secret
 data "aws_secretsmanager_secret" "secrets" {
-    name        = "robot/secrets"
-
+  name = "robot/secrets"
 }
 
-#Fetches the secrets version from the above server
-data "aws_secretsmanger_secret_version" "secret_version" {
-    secret_id  = data.aws_secretsmanager_secret.example.id
+# Fetches the secrets version from the above server
+data "aws_secretsmanager_secret_version" "secret_version" {
+  secret_id = data.aws_secretsmanager_secret.secrets.id
 }
 
 output "example" {
